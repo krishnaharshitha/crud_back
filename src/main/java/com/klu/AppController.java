9666e10827279872ffd11cb76b623fe44b063969
krishnaharshitha <2300031091@kluniversity.in>
2025-10-17 23:39:05 +0530

    Update AppController.java

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080") // allow requests from your React app
+
 public class AppController {
 
     @Autowired
